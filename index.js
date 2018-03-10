1d2b50c7fcbfdf4f1871ec04db05e9a13c5e0f94
vanessa abordo <vanessaabordo@yahoo.com>
2018-03-10 18:15:07 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const path    = require("path");
+
+const app = express();
+
+app.use(express.static('public'));
+app.engine('html', require('ejs').renderFile);
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => res.render('index.html'))
+
+app.listen(3000, () => console.log('Example app listening on port 3000!'))
