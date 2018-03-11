3c19632b1a810e82a7fd4d1585de7c94da452bca
vfaa <vanessaabordo@yahoo.com>
2018-03-11 11:15:31 +0800

    working photo clicking

--- exposedfiles/public/js/togglestuff.js
+++ exposedfiles/public/js/togglestuff.js
@@ -1,5 +1,21 @@
 $(function(){
    $("#projectTitle1").click(function(){
        $(".artworks").toggleClass("active");
+       $(".art1").toggleClass("active");
+   });
+
+   $(".art1").click(function(){
+       $(".art1").toggleClass("active");
+       $(".art2").toggleClass("active");
+   });
+
+   $(".art2").click(function(){
+       $(".art2").toggleClass("active");
+       $(".art3").toggleClass("active");
+   });
+
+   $(".art3").click(function(){
+       $(".art3").toggleClass("active");
+       $(".art1").toggleClass("active");
    });
 });
