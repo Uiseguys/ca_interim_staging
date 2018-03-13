2130202c23c545a625a2f422ffc86f5f2485808d
ahadortiz <ahadortiz@outlook.com>
2018-03-13 07:55:13 -0300

    add newsletter function fix

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
-const express = require('express');
-const path    = require("path");
+const express = require("express");
+const path = require("path");
 
 const app = express();
 
-app.use(express.static('exposedfiles'));
-app.engine('html', require('ejs').renderFile);
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
+app.use(express.static("exposedfiles"));
+app.engine("html", require("ejs").renderFile);
+app.set("views", __dirname + "/views");
+app.set("view engine", "ejs");
 
-app.get('/', (req, res) => res.render('index.html'))
+app.get("/", (req, res) => res.render("index.html"));
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'))
+app.listen(process.env.PORT || 3000, () =>
+  console.log("Example app listening on port 3000!")
+);
