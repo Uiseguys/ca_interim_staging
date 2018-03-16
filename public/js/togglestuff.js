4e86294627aab839e05611e699ee11736a43ff29
Felix Toth <felixtoth@gmail.com>
2018-03-16 12:57:51 +0100

    bugfix(styles): adjust the styles

--- public/js/togglestuff.js
+++ public/js/togglestuff.js
@@ -2,24 +2,10 @@ $(function(){
    $("#projectTitle1").click(function(){
        $(".artworks").toggleClass("active");
        $(".art1").toggleClass("active");
-       $("#actualtitle1").toggleClass("underline");
-   });
-
-   $("#projectTitle2").click(function(){
-       $("#actualtitle2").toggleClass("underline");
-   });
-
-   $("#projectTitle3").click(function(){
-       $("#actualtitle3").toggleClass("underline");
-   });
-
-   $("#projectTitle4").click(function(){
-       $("#actualtitle4").toggleClass("underline");
    });
 
    $("#newwebsite").click(function(){
        $(".emailform").toggleClass("hidden");
-       $(".websiteword").toggleClass("underline");
    });
 
    $(".art1").click(function(){
@@ -81,24 +67,6 @@ $(function(){
        }
 
    });
-
-    $(".nameHeader.intro").click(function(ev){
-
-        if($(ev.target).hasClass('intro')){
-            $("body").toggleClass("introBackground");
-            $(".titleHeader").toggleClass("intro");
-            $(".nameHeader").toggleClass("intro");
-            $(".content-container").toggleClass("hidden");
-            $(".landing").toggleClass("hidden");
-            $(".row4").toggleClass("hidden");
-            $(".ellipsis").toggleClass("hidden");
-            if($(window).width() > 1023) {
-                stickyHeaders.load($(".sectionHeader"));
-            }
-
-        }
-
-    });
 
     $(".arrow").click(function(ev) {
         $('html, body').animate({scrollTop: '0px'}, 300);
