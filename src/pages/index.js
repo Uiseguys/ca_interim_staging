f0665bb97acd0b7d74aeaa91faf2a3c6214a9beb
vanessa <vanessa@snapzio.com>
2018-03-19 18:38:48 +0800

    rough gatsby version of christian awe website

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,9 @@
 import React from 'react'
 import Link from 'gatsby-link'
+import Overall from '../components/Overall.js'
 
 export default () => (
   <div>
-    <scb-alert type='primary'>I am a stencil alert</scb-alert>
-
-    <div class='jumbotron'>
-      <h1 class='display-4'>Hello, world!</h1>
-      <p class='lead'>This is bootstrap.</p>
-    </div>
+    <Overall />
   </div>
 )
