ee9e11f053fbdee53f077aa97408b0c90077e125
vfaa <vanessaabordo@yahoo.com>
2018-03-27 19:21:12 +0800

    prepare class for artwork caption to swap position on mobile

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -14,14 +14,16 @@ class Gallery extends React.Component {
     return (
       <div className={`${styles.gallery} ${styles.col22}`}>
         <div>
-          <div className={`${styles.firstpar} ${styles.caption}`}>
-            Exhibition with works of Christian Awe<br />
-            and the Georgian artist Levan<br />
-            Songulashvilli
-          </div>
-          <div className={styles.caption}>
-            {'68projects, Fasanenstr. 68, 10719 Berlin'}<br />
-            {'Opening: 16.03., 6 - 9 p.m.'}
+          <div className={styles.wholecaption}>
+            <div className={`${styles.firstpar} ${styles.caption}`}>
+              Exhibition with works of Christian Awe<br />
+              and the Georgian artist Levan<br />
+              Songulashvilli
+            </div>
+            <div className={styles.caption}>
+              {'68projects, Fasanenstr. 68, 10719 Berlin'}<br />
+              {'Opening: 16.03., 6 - 9 p.m.'}
+            </div>
           </div>
           <Artwork />
         </div>
