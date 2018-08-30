9c921c7df1306b9f67518cc67e6cee4a39addeb7
Vanessa <vanessaabordo@yahoo.com>
2018-08-30 18:37:24 +0800

    update layers of nature

--- public/js/togglestuff.js
+++ public/js/togglestuff.js
@@ -34,6 +34,16 @@ $(function(){
         $("#art7a").toggleClass("active");
     });
 
+    $("#projectTitle8").click(function(){
+        $("#artworks8").toggleClass("active");
+        $("#art8a").toggleClass("active");
+    });
+
+    $("#projectTitle9").click(function(){
+        $("#artworks9").toggleClass("active");
+        $("#art9a").toggleClass("active");
+    });
+
    $("#newwebsite").click(function(){
        $(".emailform").toggleClass("hidden");
    });
@@ -245,6 +255,163 @@ $(function(){
     $("#art6b").on('swiperight', function(){
         $("#art6b").toggleClass("active");
         $("#art6a").toggleClass("active");
+    });
+
+    /* last Block */
+
+    $("#art9a").click(function(){
+        $("#art9a").toggleClass("active");
+        $("#art9b").toggleClass("active");
+    });
+
+    $("#art9b").click(function(){
+        $("#art9b").toggleClass("active");
+        $("#art9c").toggleClass("active");
+    });
+
+    $("#art9c").click(function(){
+        $("#art9c").toggleClass("active");
+        $("#art9d").toggleClass("active");
+    });
+
+    $("#art9d").click(function(){
+        $("#art9d").toggleClass("active");
+        $("#art9e").toggleClass("active");
+    });
+
+    $("#art9e").click(function(){
+        $("#art9e").toggleClass("active");
+        $("#art9f").toggleClass("active");
+    });
+
+    $("#art9f").click(function(){
+        $("#art9f").toggleClass("active");
+        $("#art9g").toggleClass("active");
+    });
+
+    $("#art9g").click(function(){
+        $("#art9g").toggleClass("active");
+        $("#art9h").toggleClass("active");
+    });
+
+    $("#art9h").click(function(){
+        $("#art9h").toggleClass("active");
+        $("#art9i").toggleClass("active");
+    });
+
+    $("#art9i").click(function(){
+        $("#art9i").toggleClass("active");
+        $("#art9j").toggleClass("active");
+    });
+
+    $("#art9j").click(function(){
+        $("#art9j").toggleClass("active");
+        $("#art9a").toggleClass("active");
+    });
+
+    // swipeleft
+
+    $("#art9a").on('swipeleft', function(){
+        $("#art9a").toggleClass("active");
+        $("#art9b").toggleClass("active");
+    });
+
+    $("#art9b").on('swipeleft', function(){
+        $("#art9b").toggleClass("active");
+        $("#art9c").toggleClass("active");
+    });
+
+    $("#art9c").on('swipeleft', function(){
+        $("#art9c").toggleClass("active");
+        $("#art9d").toggleClass("active");
+    });
+
+    $("#art9d").on('swipeleft', function(){
+        $("#art9d").toggleClass("active");
+        $("#art9e").toggleClass("active");
+    });
+
+    $("#art9e").on('swipeleft', function(){
+        $("#art9e").toggleClass("active");
+        $("#art9f").toggleClass("active");
+    });
+
+    $("#art9f").on('swipeleft', function(){
+        $("#art9f").toggleClass("active");
+        $("#art9g").toggleClass("active");
+    });
+
+    $("#art9g").on('swipeleft', function(){
+        $("#art9g").toggleClass("active");
+        $("#art9h").toggleClass("active");
+    });
+
+    $("#art9h").on('swipeleft', function(){
+        $("#art9h").toggleClass("active");
+        $("#art9i").toggleClass("active");
+    });
+
+    $("#art9i").on('swipeleft', function(){
+        $("#art9i").toggleClass("active");
+        $("#art9j").toggleClass("active");
+    });
+
+    $("#art9j").on('swipeleft', function(){
+        $("#art9j").toggleClass("active");
+        $("#art9a").toggleClass("active");
+    });
+
+
+    // swiperight
+
+    $("#art9a").on('swiperight', function(){
+        $("#art9a").toggleClass("active");
+        $("#art9j").toggleClass("active");
+    });
+
+    $("#art9b").on('swiperight', function(){
+        $("#art9b").toggleClass("active");
+        $("#art9a").toggleClass("active");
+    });
+
+    $("#art9c").on('swiperight', function(){
+        $("#art9c").toggleClass("active");
+        $("#art9b").toggleClass("active");
+    });
+
+    $("#art9d").on('swiperight', function(){
+        $("#art9d").toggleClass("active");
+        $("#art9c").toggleClass("active");
+    });
+
+    $("#art9e").on('swiperight', function(){
+        $("#art9e").toggleClass("active");
+        $("#art9d").toggleClass("active");
+    });
+
+    $("#art9f").on('swiperight', function(){
+        $("#art9f").toggleClass("active");
+        $("#art9e").toggleClass("active");
+    });
+
+    $("#art9g").on('swiperight', function(){
+        $("#art9g").toggleClass("active");
+        $("#art9f").toggleClass("active");
+    });
+
+    $("#art9h").on('swiperight', function(){
+        $("#art9h").toggleClass("active");
+        $("#art9g").toggleClass("active");
+    });
+
+    $("#art9i").on('swiperight', function(){
+        $("#art9i").toggleClass("active");
+        $("#art9h").toggleClass("active");
+    });
+
+    $("#art9j").on('swiperight', function(){
+        $("#art9j").toggleClass("active");
+        $("#art9i").toggleClass("active");
     });
 
 
